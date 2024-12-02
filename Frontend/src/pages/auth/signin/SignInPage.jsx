bbb1cdb3fe9ead9681526dff089a35e173f5f42a
Anuoluwapo <classicwrld99@gmail.com>
2024-12-02 11:51:34 +0100

    Added cors

--- Frontend/src/pages/auth/signin/SignInPage.jsx
+++ Frontend/src/pages/auth/signin/SignInPage.jsx
@@ -19,7 +19,7 @@ const LoginPage = () => {
 	const { mutate:Login, isPending, isError, error } = useMutation({
 		mutationFn: async ({ username, password }) => {
 			try {
-				const res = await fetch("/auth/login", {
+				const res = await fetch("/api/auth/login", {
 					method: "POST",
 					headers: {
 					"Content-Type": "application/json",
