01ce64acfa537f525ab407804a92ef138678a64d
Anuoluwapo <classicwrld99@gmail.com>
2024-12-02 11:54:29 +0100

    added new files

--- Backend/server.js
+++ Backend/server.js
@@ -23,7 +23,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 // const __dirname = path.resolve()
 
-// app.use(cors())
+app.use(cors())
 app.use(express.json({limit: "5mb"}));
 app.use(express.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
--- Frontend/src/pages/auth/signup/SignUpPage.jsx
+++ Frontend/src/pages/auth/signup/SignUpPage.jsx
@@ -22,7 +22,7 @@ const SignUpPage = () => {
 	const { mutate, isError, isPending, error } = useMutation({
 		mutationFn: async ({ email, username, fullName, password }) => {
 			try {
-				const res = await fetch("https://twitter-clone-backend-taupe.vercel.app/?vercelToolbarCode=Aie9ug32saHCvnW/api/auth/signup", {
+				const res = await fetch("https://twitter-clone-backend-taupe.vercel.app/?vercelToolbarCode=-WRVt_jUZd5HfBt/auth/signup", {
 					method: "POST",
 					headers: {
 						"Content-Type": "application/json"
