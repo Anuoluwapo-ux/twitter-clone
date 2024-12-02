b3a0ba0d33636358b4d0ed59eda0644168facccc
Anuoluwapo <classicwrld99@gmail.com>
2024-12-02 11:38:39 +0100

    Added cors

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ import path from "path"
 import express from "express"
 import dotenv from "dotenv"
 import { v2 as cloudinary } from "cloudinary"
+import cors from 'cors'
 
 import authRoutes from "./routes/authRoute.js";
 import userRoutes from "./routes/userRoute.js";
@@ -22,6 +23,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 // const __dirname = path.resolve()
 
+app.use(cors())
 app.use(express.json({limit: "5mb"}));
 app.use(express.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
